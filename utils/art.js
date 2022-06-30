872df45f4f344d9c05ec54bae67ddc53df033d6c
Hany Soliman <hany.essam1993@gmail.com>
2022-06-30 04:25:29 +0200

    fixed files paths

--- utils/art.js
+++ utils/art.js
@@ -1,32 +1,32 @@
 const art = [
     {
         img:
-            "../images/pawel-czerwinski-9N_z3nQ2S-8-unsplash.jpg",
-        title: "Deep Urban",
-        artist: "Eugenio Mininni",
-        mp3: "../music/mixkit-deep-urban-623.mp3"
+            './images/pawel-czerwinski-9N_z3nQ2S-8-unsplash.jpg',
+        title: 'Deep Urban',
+        artist: 'Eugenio Mininni',
+        mp3: './music/mixkit-deep-urban-623.mp3'
     },
     {
-        img: "../images/pawel-czerwinski-11VCmP-NdmY-unsplash.jpg",
-        title: "Cat Walk",
-        artist: "Arulo",
-        mp3: "../music/mixkit-cat-walk-371.mp3"
+        img: './images/pawel-czerwinski-11VCmP-NdmY-unsplash.jpg',
+        title: 'Cat Walk',
+        artist: 'Arulo',
+        mp3: './music/mixkit-cat-walk-371.mp3'
 
     },
     {
         img:
-            "../images/pawel-czerwinski-bi6H3ttf4Bw-unsplash.jpg",
-        title: "C.B.P.D",
-        artist: "Arulo",
-        mp3: "../music/mixkit-cbpd-400.mp3"
+            './images/pawel-czerwinski-bi6H3ttf4Bw-unsplash.jpg',
+        title: 'C.B.P.D',
+        artist: 'Arulo',
+        mp3: './music/mixkit-cbpd-400.mp3'
 
     },
     {
         img:
-            "../images/pawel-czerwinski-MeecA0PPiJM-unsplash.jpg",
-        title: "Complicated",
-        artist: "Arulo",
-        mp3: "../music/mixkit-complicated-281.mp3"
+            './images/pawel-czerwinski-MeecA0PPiJM-unsplash.jpg',
+        title: 'Complicated',
+        artist: 'Arulo',
+        mp3: './music/mixkit-complicated-281.mp3'
 
     },
 ];
